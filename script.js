d0f11a0f37337798df55be8e126f2df488e0c580
Anjuli <arbasil0223@gmail.com>
2017-03-14 00:06:12 +0000

    added a joke page

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-$(document).ready(function(){
-    //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
-    });
-    
-    
-    
-    
+ $("#answer").click(function() {
+    append("put it on my bill");
 });
+    
+    
+    
+    
