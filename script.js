e76871548551be14bab84693990eea72cae3ef58
Anjuli <arbasil0223@gmail.com>
2017-03-21 00:11:10 +0000

    last day

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+/*global $*/
  $("#answer").click(function() {
-    append("put it on my bill");
+    alert("Put it on my bill");
+ });    
+    
+$("#answer2").click(function() {
+    alert("Gets jalapeno buisness!");
 });
     
+$("#answer3").click(function() {
+    alert("Because seven ate nine!");
+ });    
     
-    
-    
+$("#answer4").click(function() {
+    alert("It gets toad away");
+});    
+$("#answer5").click(function() {
+    alert("Because they're really good at it");
+}); 
+$("#answer6").click(function() {
+    alert("B");
+}); 
+$("#answer7").click(function() {
+    alert("They planet");
+}); 
